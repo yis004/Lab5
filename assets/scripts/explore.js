f708cf64202374548a5a5f473b9eae27fc9112cb
yis004 <114468460+yis004@users.noreply.github.com>
2022-10-27 14:00:11 -0400

    Update all code to version 2.0

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,55 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
+let voices = [];
 function init() {
-  // TODO
+  var select=document.getElementById("voice-select");
+  const synth = window.speechSynthesis;
+  function populateVoiceList() {
+    
+    setTimeout(() => {
+      
+      voices = window.speechSynthesis.getVoices();
+      for (let i = 0; i < voices.length ; i++) {
+        const option = document.createElement('option');
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+          option.textContent += ' — DEFAULT';
+        }
+        option.setAttribute('data-lang', voices[i].lang);
+        option.setAttribute('data-name', voices[i].name);
+        select.appendChild(option);
+      }
+    }, 1000);
+  }
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  var buttonPlay=document.getElementsByTagName("button");
+  var input=document.getElementById("text-to-speak");
+  var selection=document.getElementById("voice-select");
+  var img=document.getElementsByTagName("img");
+  buttonPlay[0].addEventListener('click', (event) => {
+    //img[0].setAttribute("src","assets/images/smiling-open.png")
+    event.preventDefault();
+    const utterThis = new SpeechSynthesisUtterance(input.value);
+    const selectedOption = selection.selectedOptions[0].getAttribute('data-name');
+    
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis)
+    utterThis.onstart = function (event) {
+      img[0].setAttribute("src","assets/images/smiling-open.png")
+    }   
+    utterThis.onend = function (event) {
+      img[0].setAttribute("src","assets/images/smiling.png")
+    }   
+    
+  })
+  
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,55 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const jsConfetti = new JSConfetti()
+
+  var horn=document.getElementById("horn-select");
+  horn.addEventListener('change', (event) => {
+    
+    //Q1A1
+    const img=document.getElementsByTagName("img");
+    img[0].setAttribute("src","assets/images/"+event.target.value+".svg");
+    //Q2A2
+    const audioSet=document.getElementsByClassName("hidden");
+    //alert(audioSet.length);
+    audioSet[0].setAttribute("src","assets/audio/"+event.target.value+".mp3");
+  });
+
+  //Q2A1
+  var img=document.getElementsByTagName("img");
+  var slider=document.getElementById("volume");
+  slider.addEventListener('change', (event) => {
+    if (slider.value>=1 && slider.value<33){
+      img[1].setAttribute("src","assets/icons/volume-level-1.svg");
+    }else if (slider.value>=33 && slider.value<67){
+      img[1].setAttribute("src","assets/icons/volume-level-2.svg");
+    }else if (slider.value>=33 && slider.value<=100){
+      img[1].setAttribute("src","assets/icons/volume-level-3.svg");
+    }else if (slider.value==0){
+      img[1].setAttribute("src","assets/icons/volume-level-0.svg");
+    }
+  })
+  
+
+  //Q3A1
+  var buttonPlay=document.getElementsByTagName("button");
+  
+  buttonPlay[0].addEventListener('click', (event) => {
+    const audioSet=document.getElementsByClassName("hidden");
+    audioSet[0].volume = slider.value/1000;                                        // Change audio volume based on your setting
+    audioSet[0].play();
+    
+    if (audioSet[0].getAttribute("src")=="assets/audio/party-horn.mp3"){
+      
+      jsConfetti.addConfetti()
+    }
+    
+    
+  })
+  
+  
+  
+
+
+  
 }
